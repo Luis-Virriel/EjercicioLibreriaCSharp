0de910d8b6730b4508aeb7c19119fd2f77ad5ee5
Luis-Virriel <luisvirriel@gmail.com>
2024-07-13 10:18:02 -0300

    Add files via upload
    actualizando

---
+++ Clases/Funcionario.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticoBibliteca.Clases
+{
+    public class Funcionario
+    {
+        private string _nombre;
+        private int _documento;
+        private string _cargo;
+
+        public Funcionario() { }
+
+        public Funcionario(string nombre, int documento, string cargo)
+        {
+            this._nombre = nombre;
+            this._documento = documento;
+            this._cargo = cargo;
+        }
+
+        public string GetNombre()
+        {
+            return this._nombre;
+        }
+        public int GetDocumento()
+        {
+            return this._documento;
+        }
+        public string GetCargo()
+        {
+            return this._cargo;
+        }
+
+        public void SetCargo(string cargo)
+        {
+            this._cargo = cargo;
+
+        }
+        public void SetDocumento(int documento)
+        {
+            this._documento = documento;
+
+        }
+        public void SetNombre(string nombre)
+        {
+            this._nombre = nombre;
+
+        }
+    }
+}
---
+++ Clases/Libro.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticoBibliteca.Clases
+{
+    public class Libro
+    {
+        private string _titulo;
+        private string _autor;
+        private int _AnioDePublicacion;
+
+        public Libro() { }
+        public Libro(string titulo, string autor, int anio)
+        {
+            this._titulo = titulo;
+            this._autor = autor;
+            this._AnioDePublicacion = anio;
+        }
+        public void MostrarLibro()
+        {
+            Console.WriteLine($"Libro: {_titulo} \n Autor: {_autor}\n Año de publicación: {_AnioDePublicacion}");
+        }
+
+        public string GetTitulo()
+        {
+            return _titulo;
+        }
+        public string GetAutor()
+        {
+            return _autor;
+        }
+        public int GetAnio()
+        {
+            return _AnioDePublicacion;
+        }
+
+        public void SetTitulo(string titulo)
+        {
+            this._titulo = titulo;  
+        }
+        public void SetAutor(string autor)
+        {
+            this._autor = autor;
+        }
+        public void SetAnio(int anio)
+        {
+            this._AnioDePublicacion = anio;
+        }
+
+
+
+
+    }
+}
